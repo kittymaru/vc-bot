d5461d61b14f7e5e2e75737faa6867e2afc611a3
Cathy Yu <kexinyu06@gmail.com>
2026-01-07 20:07:45 +0800

    added basic slash command

--- bot.py
+++ bot.py
@@ -8,23 +8,33 @@ BOT_TOKEN = os.getenv("TOKEN")
 
 bot = discord.Bot()
 
-intents = discord.Intents.default()
-intents.message_content = True
-
-client = discord.Client(intents=intents)
-
-@client.event
+@bot.event
 async def on_ready():
-    print(f'We have logged in as {client.user}')
+    print(f"We have logged in as {bot.user}")
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
+@bot.slash_command()
+async def hello(ctx):
+    await ctx.respond("Hello!")
+
+bot.run(BOT_TOKEN)
+
+# intents = discord.Intents.default()
+# intents.message_content = True
+
+# client = discord.Client(intents=intents)
+
+# @client.event
+# async def on_ready():
+#     print(f'We have logged in as {client.user}')
+
+# @client.event
+# async def on_message(message):
+#     if message.author == client.user:
+#         return
     
-    if message.content.startswith('chuuya'):
-        await message.channel.send('i hate chuuya bro')
-    elif message.content.startswith('sans'):
-        await message.channel.send('megalovania')
+#     if message.content.startswith('chuuya'):
+#         await message.channel.send('i hate chuuya bro')
+#     elif message.content.startswith('sans'):
+#         await message.channel.send('megalovania')
 
-client.run(BOT_TOKEN)
+# client.run(BOT_TOKEN)
